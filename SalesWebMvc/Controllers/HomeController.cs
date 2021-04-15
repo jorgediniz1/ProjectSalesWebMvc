f75d221a5b27fd9821188ec603b141358af5123c
Jorge Diniz <onediniz2@gmail.com>
2021-04-15 01:47:32 -0300

    teste

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Email"] = "onediniz2@gmail.com";
+          
+            ViewData["Name"] = "Jorge Antonio Carneiro Diniz";
 
             return View();
         }
